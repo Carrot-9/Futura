94000f3c41174752036bf076f44dfbf30fe22fd5
Krod-9 <KerrodMcNeal2@gmail.com>
2024-10-14 03:30:51 -0400

    created function 'get_file_metadata'

--- src/utils/db.rs
+++ src/utils/db.rs
@@ -3,6 +3,17 @@ use sqlx::mysql::MySql;
 
 use std::env;
 use dotenv::dotenv;
+use glob::glob;
+
+struct Songs {
+    id: i32,
+    name: Option<String>,
+    file_path: Option<String>
+}
+
+fn increment( i: &mut i32) {
+    *i += 1;
+}
 
 #[tokio::main]
  pub async fn connect() -> Result<Pool<MySql>, sqlx::Error>{
@@ -24,7 +35,32 @@ CREATE TABLE IF NOT EXISTS songs (
     )
     .execute(&conn)
     .await?;
-
+ 
  Ok(conn)
 }
+
+pub async fn _get_file_metadata(_conn: Pool<MySql>) -> Result<(), sqlx::Error> {
+
+    dotenv().ok();
+
+    let mut n:i32 = 0;
+    increment(&mut n);
+
+    let my_env = env::var("THE_PATH").expect("Path Is Set.");
+
+    for entry in glob(&my_env).expect("Files Exist.") {
+        let entry = entry.expect("Unable To Get Entry");
+        let file_path = entry.display();
+        let file_name =  entry.file_name().unwrap();
+        let file_id = n;
+        let combined = format!("{:?} {:?} {:?}", file_id, file_name, file_path);
+        println!("{:?}", combined);
+        };
+
+    Ok(())
+    }
+
+
+
+
 
