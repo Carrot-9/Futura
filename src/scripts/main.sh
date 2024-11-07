00f204099e8fd3f161609c47e497612f9b8d4f94
Krod-9 <KerrodMcNeal2@gmail.com>
2024-11-07 17:34:51 -0500

    moved 'cd ROOT_DIRECTORY'  to if statement

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -4,40 +4,40 @@
 # If main.rs returns an error, the script sends an error message to database.log and exits unsucessfully 
 # If main.rs runs smoothly, all new .wav files are moved to the table 'songs' in sopranodb
 
-# Variables 
+# Loads .env variables
+source $ENV > /dev/null 2>&1;
 
+# Variables 
 LOCK_FILE=$(mktemp /tmp/lock.XXXXXX);
 trap 'rm -f "$LOCK_FILE"' EXIT
 
-current_date=$(date +%F_%T); 
+current_date=$(date +%F_%T);
+
+# Moves to root directory /soprano so alias can execute outside this directory
+cd $ROOT_DIRECTORY;
 
 # Moves to location of rust binary
-
-cd ../../target/release;
-
-# Moves stdout and stderr to LOCK_FILE
+cd $BINARY_PATH;
 
 {
 
 ./soprano.exe
 
-} > "$LOCK_FILE" 2>&1;
+} > "$LOCK_FILE" > /dev/null 2>&1;
 
-# Moves back to root directory
-
-cd ../..;
-
-# Checks Exit Code
-
+# Sends to a log to be checked
 if [[ $? -ne 0 ]]; then 
+    cd $ROOT_DIRECTORY;
     printf "$current_date\n" >> database.log
     printf "No new .wav files in table 'songs'\n" >> database.log;
-    exit 0;
 else 
+    cd $ROOT_DIRECTORY;
     printf "$current_date\n" >> database.log
     printf "New .wav file added to table 'songs'\n" >> database.log;
-    exit 0;
 fi
+
+# Indicate Success
+printf "\nScript Executed Succesfully.\n";
 
 
 
